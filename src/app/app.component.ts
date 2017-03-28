a7e353dce4033e980f665bcb17c316c0da38eae1
Igor <ig0r260892@gmail.com>
2017-03-28 03:16:50 +0100

    Add GithubService to app.component providers array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { GithubService } from './services/github.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	styleUrls: ['./app.component.scss'],
+	providers: [ GithubService ]
 })
 export class AppComponent {
 	title = 'APP COMPONENT';
