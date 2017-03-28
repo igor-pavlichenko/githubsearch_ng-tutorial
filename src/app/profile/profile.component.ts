78787a74ad674f7959cb3a4b58ef80a3128e8d97
Igor <ig0r260892@gmail.com>
2017-03-28 03:18:45 +0100

    Inject GithubService in constructor
    Use the service to get user and log it to console.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../services/github.service';
 
 @Component({
 	selector: 'profile', // tslint:disable-line
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-	constructor() { }
+	constructor(private githubService: GithubService) {
+		this.githubService.getUser().subscribe(user => {
+			console.log(user);
+		});
+	}
 
 	ngOnInit() {
 	}
