10965768758bca726391472bf5e01c94b7cf8037
Igor <ig0r260892@gmail.com>
2017-03-29 03:36:37 +0100

    Implement the searchUser() method
    The method changes service's username and then fetches profile and repos data.
    Initialize component with user as false.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,10 +8,20 @@ import { GithubService } from '../services/github.service';
 })
 export class ProfileComponent implements OnInit {
 
-	private user: any[];
+	private user: any;
 	private repos: any[];
+	private username: string;
 
 	constructor(private githubService: GithubService) {
+		this.user = false;
+	}
+
+	ngOnInit() {
+	}
+
+	searchUser() {
+		this.githubService.updateUser(this.username);
+
 		this.githubService.getUser().subscribe(user => {
 			this.user = user;
 		});
@@ -19,9 +29,6 @@ export class ProfileComponent implements OnInit {
 		this.githubService.getRepos().subscribe(repos => {
 			this.repos = repos;
 		});
-	}
-
-	ngOnInit() {
 	}
 
 }
